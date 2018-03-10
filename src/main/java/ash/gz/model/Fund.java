4e295da92b0c94a5e000929ed1fca4697fd40ebe
asherjames <asherjames.nz@gmail.com>
2018-03-10 20:29:25 +1300

    Exclude h2 from fatjar

--- src/main/java/ash/gz/model/Fund.java
+++ src/main/java/ash/gz/model/Fund.java
@@ -2,6 +2,7 @@ package ash.gz.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.util.Objects;
@@ -10,7 +11,7 @@ import java.util.Objects;
 public class Fund
 {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
   private String name;
