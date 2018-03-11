356ad4a3d93b278f4199691264035999e0897c5d
asherjames <asherjames.nz@gmail.com>
2018-03-11 19:35:10 +1300

    Disable csrf

--- src/main/java/ash/gz/security/ApplicationSecurity.java
+++ src/main/java/ash/gz/security/ApplicationSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -29,11 +30,12 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter
   @Override
   protected void configure(HttpSecurity http) throws Exception
   {
-    http.authorizeRequests()
+    http.csrf().disable()
+        .authorizeRequests()
         .anyRequest()
         .authenticated()
-        .and()
-        .httpBasic();
+        .and().httpBasic()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     http.exceptionHandling()
         .authenticationEntryPoint(
